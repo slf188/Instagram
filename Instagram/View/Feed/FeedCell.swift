a7dccb56d53f460c1d9b71dbc829424678584c28
Felipe Vallejo <stephanovallejom@gmail.com>
2021-02-08 14:39:45 -0500

    Home Feed

---
+++ Instagram/View/Feed/FeedCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCell.swift
+//  Instagram
+//
+//  Created by Felipe Vallejo on 2/8/21.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedCell()
+    }
+}
---
+++ Instagram/View/Search/SearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBar.swift
+//  Instagram
+//
+//  Created by Felipe Vallejo on 2/8/21.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar()
+    }
+}
