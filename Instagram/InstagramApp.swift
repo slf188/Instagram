562b3443e0b5c6f1bd351c74ad2800590b10b1fb
Felipe Vallejo <stephanovallejom@gmail.com>
2021-02-08 11:54:42 -0500

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Felipe Vallejo on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
